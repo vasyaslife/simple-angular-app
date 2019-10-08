d738abd7b7179618ab741d260f096bc66e4b0847
Vasiliy <vasyasjob@gmail.com>
2019-10-08 15:58:18 +0300

    finished login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { MainComponent } from './main/main.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DashItemComponent } from './dash-item/dash-item.component';
+import { AuthGuard } from './auth.guard';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    MainComponent,
+    LoginComponent,
+    DashboardComponent,
+    DashItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: LoginComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  public login = sessionStorage.getItem('login') || '';
+  public pass = sessionStorage.getItem('password') || '';
+  public passVisible = false;
+
+  logIn() {
+    if (this.checkValidation(this.login, this.pass)) {
+      this.addSessionInfo(this.login, this.pass);
+
+      this.routeToMain();
+    } else {
+      alert('Введены неправильные данные, пожалуйста введите логин длиннее трех символов. ' +
+      'Пароль не должен быть короче 10 символов, не должен содержать кириллицу, начинаться с цифры ' +
+      'и должен иметь не менее одной заглавной буквы');
+    }
+  }
+
+  checkValidation(login, pass) {
+    let passRegEx = /^\d|[А-ЯЁ]/i;
+
+    if (login.length > 3 &&
+      pass.length >= 10 &&
+      !passRegEx.test(pass) &&
+      pass.toLowerCase() !== pass) {
+      return true;
+    }
+
+    return false;
+  }
+
+  addSessionInfo(login, pass) {
+    sessionStorage.removeItem('login');
+    sessionStorage.removeItem('password');
+
+    sessionStorage.setItem('login', login);
+    sessionStorage.setItem('password', pass);
+  }
+
+  routeToMain() {
+    window.location.href = '/main';
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
